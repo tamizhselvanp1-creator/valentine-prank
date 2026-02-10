ec02417bf2a620c10ec6243ac81edd879948e4ad
tamizhselvanp1-creator <tamizhselvanp1@gmail.com>
2026-02-10 23:15:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const noBtn = document.getElementById("noBtn");
 const card = document.getElementById("card");
 const finalScreen = document.getElementById("finalScreen");
 
-let noCount = 0;
+let noCount = 4;
 const maxNo = 5;
 
 // Added a smooth transition so the move and grow look fluid
@@ -46,3 +46,4 @@ noBtn.addEventListener("click", () => {
     noBtn.style.display = "none";
   }
 });
+
